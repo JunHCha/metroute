68dcafc994ce0985d62fae816cd02a5bd7730266
JunHyung Cha <41867381+JunHCha@users.noreply.github.com>
2023-06-04 23:00:58 +0900

    ⚒️ Add ai-plugin.json [METROUTE-7] (#1)
    * feat: add ai-plugin.json
    
    * feat: add retrieving logo.png api

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
 
 app = FastAPI(title="metroute", openapi_url="/openapi.json")
-
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
 
 
 @app.get("/ping")
 async def ping():
     return {"message": "pong"}
+
+
+@app.get("/logo.png", response_class=FileResponse)
+async def logo():
+    filename = "logo.png"
+    return filename
